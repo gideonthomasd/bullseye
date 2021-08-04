1e9d7cc7fe486d96330a9fe9f3e1ac501e6a1ad0
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2021-08-04 16:38:32 +0100

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -4,7 +4,7 @@ sudo apt install xorg xserver-xorg xutils mesa-utils xinit openbox obconf i3 dme
 
 sudo apt install tint2 xcompmgr geany unzip build-essential pulseaudio pavucontrol lightdm fonts-font-awesome rofi gsimplecal curl lemonbar sxhkd wget youtube-dl plank lxtask lxpolkit vlc spectrwm -yy
 
-sudo apt install parcellite lxterminal vim i3blocks breeze-cursor-theme breeze-gtk-theme breeze-icon-theme gparted flatpak -yy
+sudo apt install parcellite lxterminal vim i3blocks breeze-cursor-theme breeze-gtk-theme breeze-icon-theme gparted ark flatpak -yy
 
 sudo apt install polybar -yy
 
